f3c47b9239fc45b84198b50aa7c5cf2f1fe108e5
mohamedakelportfolio <sci.m.s.hassan@gmail.com>
2022-03-03 15:18:03 +0200

    getting-started-with-pytest

---
+++ 1-getting-started-with-pytest/lessons/arithemetic_test_passing.py
@@ -0,0 +1,47 @@
+
+def test_addition_passing():
+
+    # two variables with assignments
+    a = 5
+    b = 3
+
+
+    assert(a + b) == (a + b)
+
+def test_subtraction_passing():
+
+    # two variables with assignments
+    a = 5
+    b = 3
+
+    assert(a - b) == (a - b)
+
+
+def test_multiplication_passing():
+
+    # two variables with assignments
+    a = 2
+    b = 6
+
+    assert(a * b) == (a * b)
+
+
+def test_floordividing_passing():
+
+    # two variables with assignments
+    a = 11
+    b = 2
+
+    assert(a // b) == (a // b)
+
+
+
+def test_dividing_passing():
+    
+    # two variables with assignments
+    a = 11
+    b = 2
+
+    assert(a / b) == (a / b)
+
+
